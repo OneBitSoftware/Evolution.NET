ac4657079e5db0802d924611e2758484704ff9e8
Tony Troeff <tonyslav_troeff@outlook.com>
2024-04-10 11:18:59 +0300

    Added examples with keyed services.

--- DI.Keyed/Program.cs
+++ DI.Keyed/Program.cs
@@ -13,10 +13,23 @@ public static class Program
 
         using var scope = rootServiceProvider.CreateScope();
 
+        var allNotificationServices = scope.ServiceProvider.GetServices<INotificationService>().ToArray();
+        Console.WriteLine($"There are {allNotificationServices.Length} registered non-keyed services: {string.Join(", ", allNotificationServices.Select(x => TypeNames.Get(x.GetType())))}");
+
+        var allKeyedNotificationServices = scope.ServiceProvider.GetKeyedServices<INotificationService>("_default");
+        Console.WriteLine($"There are {allNotificationServices.Length} registered services for the \"_default\" key: {string.Join(", ", allNotificationServices.Select(x => TypeNames.Get(x.GetType())))}");
+
         // The following line will always resolve the last registered service of type INotificationService..
-        var notificationService = scope.ServiceProvider.GetRequiredService<INotificationService>();
-        Console.WriteLine($"The type of resolved notification service is: {TypeNames.Get(notificationService.GetType())}");
-        await notificationService.SendAsync("Tony Troeff", "Hello, world!");
+        var defaultNotificationService = scope.ServiceProvider.GetRequiredService<INotificationService>();
+        Console.WriteLine($"The type of the default non-keyed notification service is: {TypeNames.Get(defaultNotificationService.GetType())}");
+        await defaultNotificationService.SendAsync("Tony Troeff", "Hello, world!");
+
+        Console.Write("Enter the type of notification to send: ");
+        var notificationType = Console.ReadLine()!;
+
+        var requestedNotificationService = scope.ServiceProvider.GetRequiredKeyedService<INotificationService>(notificationType);
+        Console.WriteLine($"The type of the requested notification service is: {TypeNames.Get(requestedNotificationService.GetType())}");
+        await requestedNotificationService.SendAsync("Tony Troeff", "Hello, world!");
     }
 
     private static ServiceProvider BuildServices()
@@ -24,8 +37,12 @@ public static class Program
         var serviceCollection = new ServiceCollection();
         serviceCollection.AddScoped<INotificationService, SmsNotificationService>();
         serviceCollection.AddScoped<INotificationService, EmailNotificationService>();
-        // serviceCollection.AddKeyedScoped<INotificationService, SmsNotificationService>("sms");
-        // serviceCollection.AddKeyedScoped<INotificationService, EmailNotificationService>("email");
+
+        serviceCollection.AddKeyedScoped<INotificationService, SmsNotificationService>("_default");
+        serviceCollection.AddKeyedScoped<INotificationService, EmailNotificationService>("_default");
+
+        serviceCollection.AddKeyedScoped<INotificationService, SmsNotificationService>("sms");
+        serviceCollection.AddKeyedScoped<INotificationService, EmailNotificationService>("email");
 
         var buildOptions = new ServiceProviderOptions { ValidateOnBuild = true, ValidateScopes = true };
         return serviceCollection.BuildServiceProvider(buildOptions);
